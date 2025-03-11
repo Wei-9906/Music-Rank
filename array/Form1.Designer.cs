74242bcbe570e48f51cdbf0f9f25166fc674b61b
Wei-9906 <lkjh11155038@lkjh.tp.edu.tw>
2025-03-11 16:11:47 +0800

    Add files via upload

---
+++ array/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace array
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox2 = new TextBox();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Microsoft JhengHei UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(42, 29);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(383, 482);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft JhengHei UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(459, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(247, 87);
+            button1.TabIndex = 1;
+            button1.Text = "排名";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft JhengHei UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(459, 127);
+            button2.Name = "button2";
+            button2.Size = new Size(247, 87);
+            button2.TabIndex = 2;
+            button2.Text = "歌曲排序";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft JhengHei UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(459, 236);
+            button3.Name = "button3";
+            button3.Size = new Size(247, 87);
+            button3.TabIndex = 3;
+            button3.Text = "歌手排序";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(459, 466);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(247, 45);
+            textBox2.TabIndex = 4;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(726, 22);
+            button4.Name = "button4";
+            button4.Size = new Size(67, 511);
+            button4.TabIndex = 5;
+            button4.Text = "清除";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(459, 344);
+            button5.Name = "button5";
+            button5.Size = new Size(247, 87);
+            button5.TabIndex = 6;
+            button5.Text = "查詢";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(805, 545);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(textBox2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox2;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ array/Form1.cs
@@ -0,0 +1,89 @@
+namespace array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        string[] song = new string[] { "稻香", "屋梅子醬", "在樹上唱歌", "溫柔", "好不容易" };
+        string[] singer = new string[] { "周杰倫", "李榮浩", "郭靜", "五月天", "告五人" };
+        int[] no = new int[] { 1, 2, 3, 4, 5 };
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            int[] no2 = new int[no.Length];
+            no.CopyTo(no2, 0);
+            Array.Sort(no, song);
+            Array.Sort(no2, singer);
+
+
+            for (int i = 0; i < no.Length; i++)
+            {
+                textBox1.Text +=$"{no[i]}\t{singer[i]}\t{song[i]}" + Environment.NewLine;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            string[] song2 = new string[song.Length];
+            song.CopyTo(song2, 0);
+            Array.Sort(song, no);
+            Array.Sort(song2, singer);
+
+
+            for (int i = 0; i < no.Length; i++)
+            {
+                textBox1.Text +=$"{no[i]}\t{singer[i]}\t{song[i]}" + Environment.NewLine;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            string[] singer2 = new string[singer.Length];
+            singer.CopyTo(singer2, 0);
+            Array.Sort(singer, no);
+            Array.Sort(singer2, song);
+
+
+            for (int i = 0; i < no.Length; i++)
+            {
+                textBox1.Text +=$"{no[i]}\t{singer[i]}\t{song[i]}" + Environment.NewLine;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string search = textBox2.Text;
+            string msg = $"找不到{search}";
+            int index = Array.IndexOf(singer, search);
+            //當index找不到時 為-1
+            if (index >= 0)
+            {
+                msg = "排名\t歌手\t歌曲" + Environment.NewLine;
+                while (index >= 0) //只要index>=0就一直執行此迴圈
+                {
+                    msg += $"{no[index]}\t{singer[index]}\t{song[index]}" + Environment.NewLine;
+                    index = Array.IndexOf(singer, search, index + 1);
+                }
+            }
+            MessageBox.Show(msg, "查詢結果"); //查詢結果為msgbox的title
+        }
+    }
+}
